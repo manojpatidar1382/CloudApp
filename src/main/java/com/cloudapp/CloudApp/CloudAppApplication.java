5716c151fab7706a4325de33968a628e22c3a1da
manojpatidar1382 <manojpatidar1381@gmail.com>
2023-11-27 03:36:03 +0530

    commit

--- src/main/java/com/cloudapp/CloudApp/CloudAppApplication.java
+++ src/main/java/com/cloudapp/CloudApp/CloudAppApplication.java
@@ -2,12 +2,20 @@ package com.cloudapp.CloudApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CloudAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudAppApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello";
 	}
 
 }
